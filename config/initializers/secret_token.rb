e2bf47be4a4d2f5872f9434a84c2d21aac04c135
Johan Eckerström <johan@duh.se>
2010-07-29 11:23:46 +0200

    Rails 3.0RC

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Linticket::Application.config.secret_token = '0ef1c73fd5f143a17aa490bb718e42c2a3cbf14420d42ba01b4f5015ff61de9efaa638f0fd506e0673ce450cd6b2929d8cb889c1b6722be1a50988f31be5ee6c'

d84336a27a4dd71b460522cb3bc73d8952c018f6
Johan Eckerström <johan@duh.se>
2010-07-29 21:58:25 +0200

    Preload event

--- app/controllers/registration_batches_controller.rb
+++ app/controllers/registration_batches_controller.rb
@@ -1,30 +1,32 @@
 class RegistrationBatchesController < ApplicationController  
+  
+  before_filter :load_event
+  
   def index
-    @event = Event.find(params[:event_id])
     @registration_batches = @event.registration_batches
   end
   
   def show
-    @event = Event.find(params[:event_id])
     @registration_batch = @event.registration_batches.find(params[:id])
     @visitors = @registration_batch.visitors
   end
   
   def new
-    @event = Event.find(params[:event_id])
     @registration_batch = RegistrationBatch.new
   end
   
   def create
-    @event = Event.find(params[:event_id])
     @event.registration_batches << RegistrationBatch.new(params[:registration_batch])
     redirect_to(event_registration_batches_path(@event))
   end
   
   def destroy
-    @event = Event.find(params[:event_id])
     @registration_batch = @event.registration_batches.find(params[:id])
     @registration_batch.destroy
     redirect_to(event_registration_batches_path(@event), :notice => "Batch raderad")
+  end
+private
+  def load_event
+    @event = Event.find(params[:event_id])
   end
 end

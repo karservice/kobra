6d1aa2567d0a33869cee4bcdd80e7e30d9d3abbf
Johan Eckerström <johan@duh.se>
2010-09-09 23:36:34 +0200

    Convert to string and handle personal numbers better

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,8 @@
 class Student < ActiveRecord::Base
   scope :search, lambda { |keyword|
+      # Convert to string
+      keyword = keyword.to_s
+      
       # Searchable keys
       # These keys should have an index in the database for performance
       keys = [:first_name, :last_name, :personal_number, :rfid_number, :barcode_number, :email]
@@ -7,18 +10,14 @@ class Student < ActiveRecord::Base
       if not keyword.to_s.strip.empty?
         # Handle different personal number styles
         #  19860421-0000
-        #  198604210000
         #  860421-0000 (don't do anything, just notice)
-        #  8604210000
-        # Records stored as 860421-0000 in student database
+        # Don't handle numbers without hyphen, don't want to screw with RFID or barcode numbers
+        #
+        # Personal number recors stored as 860421-0000 in student database
         if m = keyword.match(/^(\d{2})(\d{6}-\d{4})$/) # 19860421-0000
           keyword = m[2]
-        elsif m = keyword.match(/^(\d{2})(\d{6})(\d{4})$/) # 198604210000
-          keyword = m[2] + "-" + m[3]
         elsif m = keyword.match(/^(\d{6}-\d{4})$/) # 860421-0000
           # Just notice for later performance tweak
-        elsif m = keyword.match(/^(\d{6})(\d{4})$/) # 8604210000
-          keyword = m[1] + "-" + m[2]
         end
         # If there is a match, we can just look at personal_number column to speed things up a bit
         keys = [:personal_number] if m

6e0bdfa84d07da33f50a269addf1dbdc542252f4
Johan Eckerström <johan@duh.se>
2010-09-23 23:11:35 +0200

    Sync with studentkoll

--- app/models/visitor.rb
+++ app/models/visitor.rb
@@ -3,12 +3,12 @@ class Visitor < ActiveRecord::Base
   has_many :registrations, :dependent => :destroy # FIXME visitor should never be deleted
   has_many :tickets, :through => :registrations
   has_many :events, :through => :registrations
-  
+
   scope :search, lambda { |keyword|
       # Searchable keys
       # These keys should have an index in the database for performance
       keys = [:first_name, :last_name, :personal_number, :rfid_number, :barcode_number, :email]
-      
+
       if not keyword.to_s.strip.empty?
         # Handle different personal number styles
         #  19860421-0000
@@ -18,7 +18,7 @@ class Visitor < ActiveRecord::Base
         # Records stored as 860421-0000 in student database
         if m = keyword.match(/^(\d{2})(\d{6}-\d{4})$/) # 19860421-0000
           keyword = m[2]
-        elsif m = keyword.match(/^(\d{2})(\d{6})(\d{4})$/) # 198604210000 
+        elsif m = keyword.match(/^(\d{2})(\d{6})(\d{4})$/) # 198604210000
         #  keyword = m[2] + "-" + m[3]
         elsif m = keyword.match(/^(\d{6}-\d{4})$/) # 860421-0000
           # Just notice for later performance tweak
@@ -39,44 +39,41 @@ class Visitor < ActiveRecord::Base
         {:limit => 0}
       end
     }
-  
+
   attr_accessor :query # For queries
-  
-  # Sets attributes from student
+
+  # Sets attributes from Studentkoll student
   def set_attributes_from_student(student)
-    visitor_attributes = Visitor.column_names
-    student_attributes = student.attributes.keys
-    
-    # Take all attributes that are the same, this requires Ruby 1.9
-    student_attributes = student.attributes.select do |k,v|
-      visitor_attributes.include?(k)
-    end
-    self.attributes = student_attributes
+    self.first_name     = student.fornamn
+    self.last_name      = student.efternamn
+    self.rfid_number    = student.rfidnr
+    self.barcode_number = student.streckkodnr
+    self.email          = student.epost
   end
 
   # Should be split up in a few delayed jobs for parallelization
   def self.sync_from_students
     # FIXME benchmark and use the best batch_size
     Visitor.find_each(:batch_size => 100) do |visitor|
-      if student = Student.where(:personal_number => visitor.personal_number).first
+      if student = Studentkoll.where(:pnr_format => visitor.personal_number).first
         visitor.set_attributes_from_student(student)
         visitor.save
       end
     end
   end
-  
+
   def name
     "#{self.first_name} #{self.last_name}"
   end
-  
+
   def to_s
     self.name
   end
-  
+
   def union_member?
     union || union_override
   end
-  
+
   def union
     if result = StureStudent.where(:personal_number => self.personal_number).first
       result.student_union

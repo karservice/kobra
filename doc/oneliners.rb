915b07f19c041f329c98836b274f4f43f201b483
Johan Eckerström <johan@duh.se>
2010-09-23 17:04:36 +0200

    More oneliners

--- doc/oneliners.rb
+++ doc/oneliners.rb
@@ -1,3 +1,4 @@
+# -*- encoding : utf-8 -*-
 # Dumpa Ladok
 File.open('ladokdump', 'w') do |f|
   Liukort.find(:all).each{ |l| f.write(l.attributes.to_yaml) }
@@ -44,13 +45,102 @@ Ticket.count - Ticket.where(:handed_out_at => nil).count
 
 # Lägga in sture databas
 sture_students = []
-File.read("/tmp/medlem_20100917.txt").each_line {|l| m = /(\d{2})(\d{6})(\w{4}) \| (\w*)/.match(l); sture_students << [ "#{m[2]}-#{m[3]}", m[4] ] }.nil?
+File.read("/Users/jage/Desktop/medlem_20100922.txt").each_line {|l| m = /(\d{2})(\d{6})(\w{4}) \| (\w*)/.match(l); sture_students << [ "#{m[2]}-#{m[3]}", m[4] ] }.nil?
 sture_students.each {|s|
   unless StureStudent.where(:personal_number => s[0]).first
     StureStudent.create(:personal_number => s[0], :student_union => s[1])
   end
 }
 
-new_students.each {|s| 
+new_students.each {|s|
   StureStudent.create(:personal_number => s.pnr_format, :student_union => "LinTek")
 }
+
+sture_students_18 = []
+File.read("/tmp/medlem_20100918.txt").each_line {|l| m = /(\d{2})(\d{6})(\w{4}) \| (\w*)/.match(l); sture_students_18 << [ "#{m[2]}-#{m[3]}", m[4] ] }.nil?
+
+sture_students_17 = []
+File.read("/tmp/medlem_20100917.txt").each_line {|l| m = /(\d{2})(\d{6})(\w{4}) \| (\w*)/.match(l); sture_students_17 << [ "#{m[2]}-#{m[3]}", m[4] ] }.nil?
+
+sture_diff = sture_students_18 - sture_students_17
+
+
+sture_diff.each {|sd|
+  print sd[0]
+  print "\t"
+  print sd[1]
+  print "\t"
+  od = Studentkoll.where(:pnr_format => sd).first
+  if od
+    if od.kar
+      print od.kar
+    else
+      print "-"
+    end
+    "\t"
+    print od.epost
+  else
+    print "-"
+    "#\t"
+    print "-"
+  end
+  print "\n"
+}
+
+# Kårer
+Consensus = ["At  ",
+ "BMA ",      
+ "Cons",      
+ "Domf",      
+ "Fri ",      
+ "Log ",      
+ "Läk ",      
+ "MedB",      
+ "SG  ",      
+ "SskL",      
+ "SskN",      
+ "Stöd"]      
+              
+LinTek = ["C   ",      
+ "CTD ",      
+ "D   ",      
+ "DokL",      
+ "ED  ",      
+ "FriL",      
+ "GDK ",      
+ "I   ",      
+ "KTS ",      
+ "Ling",      
+ "LinT",      
+ "M   ",      
+ "MatN",      
+ "MT  ",      
+ "N   ",      
+ "StöL",      
+ "TBI ",      
+ "Y   ",      
+              
+StuFF = ["AJF ",      
+ "Dokt",      
+ "flin",      
+ "FriS",      
+ "Gans",      
+ "KogV",      
+ "MiP ",      
+ "PULS",      
+ "SAKS",      
+ "SKA ",      
+ "SKUM",      
+ "Soci",      
+ "SSHF",      
+ "Stal",      
+ "STiL",      
+ "Stim",      
+ "StuF"]     
+              
+
+
+
+
+
+

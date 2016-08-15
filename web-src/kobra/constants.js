b71b153a52cbe3d1988c291d51856fde09477007
Olle Vidner <olle@vidner.se>
2016-08-16 00:38:12 +0200

    Changed JWT refresh margin to something more reasonable

--- web-src/kobra/constants.js
+++ web-src/kobra/constants.js
@@ -3,4 +3,4 @@ export const apiRoot = '/api/v1'
 export const localJwtKey = 'jwt'
 
 // Number of milliseconds before JWT expiry the token should be refreshed
-export const jwtRefreshMargin = 290000
+export const jwtRefreshMargin = 60000

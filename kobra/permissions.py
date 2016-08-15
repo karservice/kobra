d6605c1f17d9d7e059c05a53f129e3a426347b3e
Olle Vidner <olle@vidner.se>
2016-08-15 22:45:01 +0200

    Made the permissions and predicates system a bit more tidy and robust

--- kobra/permissions.py
+++ kobra/permissions.py
@@ -4,45 +4,49 @@ import rules
 from . import predicates
 
 
-defaults = rules.is_authenticated & (
-    predicates.always_allow_model_perm | rules.is_superuser)
+is_authenticated_model_level = rules.is_authenticated & predicates.is_model_level
 
-rules.add_perm('kobra.add_discount', defaults)
+rules.add_perm('kobra.add_discount', is_authenticated_model_level)
 rules.add_perm('kobra.view_discount',
-               (defaults | predicates.is_discount_admin))
+               (is_authenticated_model_level | predicates.is_discount_admin))
 rules.add_perm('kobra.change_discount',
-               (defaults | predicates.is_discount_admin))
+               (is_authenticated_model_level | predicates.is_discount_admin))
 rules.add_perm('kobra.delete_discount',
-               (defaults | predicates.is_discount_admin))
+               (is_authenticated_model_level | predicates.is_discount_admin))
 
-rules.add_perm('kobra.add_discountregistration', defaults)
+rules.add_perm('kobra.add_discountregistration', is_authenticated_model_level)
 rules.add_perm('kobra.view_discountregistration',
-               (defaults | predicates.is_discount_registration_admin))
+               (is_authenticated_model_level |
+                predicates.is_discount_registration_admin))
 rules.add_perm('kobra.change_discountregistration',
-               (defaults | predicates.is_discount_registration_admin))
+               (is_authenticated_model_level |
+                predicates.is_discount_registration_admin))
 
-rules.add_perm('kobra.add_event', defaults)
-rules.add_perm('kobra.view_event', (defaults | predicates.is_event_admin))
-rules.add_perm('kobra.change_event', (defaults | predicates.is_event_admin))
-rules.add_perm('kobra.delete_event', (defaults | predicates.is_event_admin))
+rules.add_perm('kobra.add_event', is_authenticated_model_level)
+rules.add_perm('kobra.view_event', (is_authenticated_model_level |
+                                    predicates.is_event_admin))
+rules.add_perm('kobra.change_event', (is_authenticated_model_level |
+                                      predicates.is_event_admin))
+rules.add_perm('kobra.delete_event', (is_authenticated_model_level |
+                                      predicates.is_event_admin))
 
-# rules.add_perm('kobra.add_organization', defaults)
 rules.add_perm('kobra.view_organization',
-               (defaults | predicates.is_organization_admin))
+               (is_authenticated_model_level |
+                predicates.is_organization_admin))
 rules.add_perm('kobra.change_organization',
-               (defaults | predicates.is_organization_admin))
-# rules.add_perm('kobra.delete_organization', defaults)
+               (is_authenticated_model_level |
+                predicates.is_organization_admin))
 
 rules.add_perm('kobra.view_section', rules.is_authenticated)
 
 rules.add_perm('kobra.view_student', rules.is_authenticated)
 
-rules.add_perm('kobra.add_tickettype', defaults)
+rules.add_perm('kobra.add_tickettype', is_authenticated_model_level)
 rules.add_perm('kobra.view_tickettype',
-               (defaults | predicates.is_ticket_type_admin))
+               (is_authenticated_model_level | predicates.is_ticket_type_admin))
 rules.add_perm('kobra.change_tickettype',
-               (defaults | predicates.is_ticket_type_admin))
+               (is_authenticated_model_level | predicates.is_ticket_type_admin))
 rules.add_perm('kobra.delete_tickettype',
-               (defaults | predicates.is_ticket_type_admin))
+               (is_authenticated_model_level | predicates.is_ticket_type_admin))
 
 rules.add_perm('kobra.view_union', rules.is_authenticated)
--- kobra/predicates.py
+++ kobra/predicates.py
@@ -2,8 +2,10 @@
 import rules
 
 
+# Model level (i.e. with obj=None) permission requests are used for list
+# requests in the API, so we want to allow those in a convenient manner.
 @rules.predicate
-def always_allow_model_perm(user, obj):
+def is_model_level(user, obj):
     if obj is None:
         return True
     return False
@@ -11,24 +13,34 @@ def always_allow_model_perm(user, obj):
 
 @rules.predicate
 def is_organization_admin(user, organization):
+    if organization is None:
+        return False
     return organization.admins.filter(pk=user.pk).exists()
 
 
 @rules.predicate
 def is_event_admin(user, event):
+    if event is None:
+        return False
     return is_organization_admin(user, event.organization)
 
 
 @rules.predicate
 def is_ticket_type_admin(user, ticket_type):
+    if ticket_type is None:
+        return False
     return is_event_admin(user, ticket_type.event)
 
 
 @rules.predicate
 def is_discount_admin(user, discount):
+    if discount is None:
+        return False
     return is_ticket_type_admin(user, discount.ticket_type)
 
 
 @rules.predicate
 def is_discount_registration_admin(user, discount_registration):
+    if discount_registration is None:
+        return False
     return is_discount_admin(user, discount_registration.discount)

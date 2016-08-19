eca32a727cccc81aa2b940c14c6f1b53365b6a52
Olle Vidner <olle@vidner.se>
2016-08-19 10:57:19 +0200

    Made the start script fail on certain non-zero exit statuses

--- run-django.sh
+++ run-django.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
+# Fail on non-zero exit status
+set -e
+
 # Collect static content for Django
 django-admin collectstatic --noinput
+
+# Don't fail on non-zero exit status (we don't care that much about Opbeat)
+set +e
 
 # Report backend release to Opbeat
 curl https://intake.opbeat.com/api/v1/organizations/${OPBEAT_BACKEND_ORGANIZATION_ID}/apps/${OPBEAT_BACKEND_APP_ID}/releases/ \
@@ -17,5 +23,8 @@ curl https://intake.opbeat.com/api/v1/organizations/${OPBEAT_FRONTEND_ORGANIZATI
   -d branch=`git rev-parse --abbrev-ref HEAD` \
   -d machine=`hostname` \
   -d status=machine-completed
+
+# Fail on non-zero exit status
+set -e
 
 gunicorn kobra.wsgi -c /src/gunicorn-conf.py

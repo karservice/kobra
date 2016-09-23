8765c4b7a58923ace2c33fa6e28c52192424dcb3
Olle Vidner <olle@vidner.se>
2016-09-23 13:16:31 +0200

    Changed gunicorn default log level to give more confirmation on start

--- gunicorn-conf.py
+++ gunicorn-conf.py
@@ -13,6 +13,6 @@ bind = ['0.0.0.0:8000']
 reload = env.bool('GUNICORN_RELOAD', False)
 workers = env.int('GUNICORN_WORKERS', cpu_count()*2+1)
 
-loglevel = env.str('GUNICORN_LOG_LEVEL', 'error')
+loglevel = env.str('GUNICORN_LOG_LEVEL', 'info')
 errorlog = '-'  # stderr
 accesslog = '-' if env.bool('GUNICORN_ACCESS_LOG', False) else None

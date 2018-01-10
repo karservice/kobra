fef1993cf37b5d3c11948e4246569f25551fc1ff
Olle Vidner <olle@vidner.se>
2018-01-10 21:26:18 +0100

    Added bin/ to $PATH instead of resorting to setup.py hack

--- setup.py
+++ setup.py
@@ -2,17 +2,10 @@ import os
 
 from setuptools import setup
 
-
-def find_binaries():
-    bin_dir = os.path.join(os.path.dirname(__file__), 'bin')
-    return [os.path.join(bin_dir, x) for x in os.listdir(bin_dir)]
-
-
 setup(
     name='kobra',
     version='1.0.0',
     packages=['kobra'],
-    scripts=find_binaries(),
     url='https://github.com/karservice/kobra',
     license='MIT',
     author='Kårservice Östergötland AB',
